c561ecf342e32e200a69dd8c6fd21c04ada1b62c
Alan <breder.pnd@gmail.com>
2026-03-04 15:24:10 -0300

    user

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 import hashlib
-from fastapi import HTTPException
+from fastapi import HTTPException, Header
 from app.database import engine, SessionLocal, Base
 from app.models import User
 import app.models as models
@@ -45,3 +45,19 @@ def login(username: str, password: str, db: Session = Depends(get_db)):
             status_code=401, detail="Usuário ou senha inválidos")
 
     return {"msg": "Login realizado com sucesso"}
+
+
+def verificar_usuario(username: str = Header(...), password: str = Header(...), db: Session = Depends(get_db)):
+    senha_hash = hashlib.sha256(password.encode()).hexdigest()
+
+    user = db.query(User).filter(User.username == username).first()
+
+    if not user or user.password != senha_hash:
+        raise HTTPException(status_code=401, detail="Não autorizado")
+
+    return user
+
+
+@app.get("/protegido")
+def protegido(user: User = Depends(verificar_usuario)):
+    return {"msg": "Acesso autorizado"}
