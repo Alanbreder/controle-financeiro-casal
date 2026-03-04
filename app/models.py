4a23c769db72fd50a9fb09f9a6c794a546c557c7
Alan <breder.pnd@gmail.com>
2026-03-04 15:57:47 -0300

    Restaurando model user e lancamento

--- app/models.py
+++ app/models.py
@@ -3,6 +3,14 @@ from app.database import Base
 from app.models import User
 
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+
+
 class Lancamento(Base):
     __tablename__ = "lancamentos"
 
