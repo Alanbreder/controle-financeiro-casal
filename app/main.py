dc0a132154ca38f3973ceeaf85e2d1f7bd1c7606
Alan <breder.pnd@gmail.com>
2026-03-04 11:45:24 -0300

    estrutura inicial

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.database import engine, Base
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def home():
+    return {"status": "Sistema rodando"}
